c1a039154003b44418bf55a3ff73cf0b6feb2503
Bokis Angelov <bokisangelov@gmail.com>
2025-08-18 13:59:26 +0300

    Add models in Admin portal

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UserProfile, Excursion, ExcursionAvailability, Booking, Transaction, Feedback, Category, Tag, Group, PaymentMethod
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Excursion)
+admin.site.register(ExcursionAvailability)
+admin.site.register(Booking)
+admin.site.register(Transaction)
+admin.site.register(Feedback)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Group)
+admin.site.register(PaymentMethod)
